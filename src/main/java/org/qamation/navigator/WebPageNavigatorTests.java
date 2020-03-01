1270e95cd93b9c8d4d76cdb46d73b853abaebea6
Pavel <gpawel@gmail.com>
2020-02-29 20:53:24 -0500

    initial commit

---
+++ src/main/java/org/qamation/navigator/WebPageNavigatorTests.java
@@ -0,0 +1,84 @@
+package org.qamation.navigator;
+
+
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.qamation.web.page.Page;
+import org.qamation.web.page.WebPageFactory;
+import org.qamation.webdriver.utils.LocatorFactory;
+import org.qamation.webdriver.utils.WebDriverFactory;
+
+
+import java.util.List;
+
+public class WebPageNavigatorTests {
+    WebPageNavigator navigator;
+    WebDriver driver;
+    Page page;
+
+    @Before
+    public void setUp() {
+        String path = System.getProperty("user.dir")+"/../driver/chromedriver";
+        driver = WebDriverFactory.createChromeWebDriver(path);
+        navigator = new WebPageNavigator(driver);
+        page = WebPageFactory.createGeneralPageInstance(driver);
+
+    }
+
+    /*
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+*/
+
+    @Test
+    public void openYahooPage() {
+        page.openPage("http://yahoo.com");
+        page.isReady();
+        navigator.processNavigationSequence(new String[]{"moon","{ENTER}"},page);
+        assertTrue(driver.getPageSource().contains("<b>Moon </b>- Image Results"));
+    }
+
+    @Test
+    public void processNavigationStringWikipedia() {
+        page.openPage("http://wikipedia.com");
+        page.isReady();
+        String[] navigation = new String[] {"qa <@!{xpath=//*[@id='search-form']/fieldset/button}>"};
+        WebPageNavigator navigator = new WebPageNavigator(driver);
+        navigator.processNavigationSequence(navigation,page);
+        By by = LocatorFactory.getLocator("firstHeading");
+        List<WebElement> els = driver.findElements(by);
+        Assert.assertNotNull(els);
+        Assert.assertTrue(els.size()==1);
+        Assert.assertEquals("QA",els.get(0).getText());
+        //NavigationString navigationString = new NavigationString()
+    }
+
+    @Test
+    public void processNavigationStringAmazon() {
+        page.openPage("http://amazon.ca");
+        page.isReady();
+        WebPageNavigator navigator = new WebPageNavigator(driver);
+        navigator.processNavigationSequence(new String[] {"<@!{xpath=//*[contains(text(),'Hello. Sign in')]}>"},page);
+        List<WebElement> els = driver.findElements(LocatorFactory.getLocator("xpath=//h1[contains(text(),'Sign in')]"));
+        Assert.assertEquals(1,els.size());
+        Assert.assertEquals("Sign in",els.get(0).getText());
+        navigator.processNavigationSequence(new String[] {"gpawel17@mail.com","1qazxsw2!"}, page);
+        navigator.processNavigationSequence(new String[] {"<@!{xpath=//div[@class='nav-search-field ']}>"}, page);
+        navigator.processNavigationSequence(new String[] {"hair {SPACE} brash"}, page );
+        els = driver.findElements(LocatorFactory.getLocator("xpath=//*[@id=’didYouMean’]"));
+        Assert.assertEquals(1,els.size());
+        Assert.assertEquals("Showing results for",els.get(0).getText());
+
+    }
+
+
+
+
+}
